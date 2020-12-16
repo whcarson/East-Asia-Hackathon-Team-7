9a2deaa89c32355d8a5efe7c0aa8795e7a7d7a98
whcarson <whcarson@uchicago.edu>
2020-12-16 15:39:11 -0500

    Added some framework functionality using react

---
+++ src/MainPage.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+class User extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            _id: null,
+            _lat: null,
+            _lon: null,
+        }
+    }
+    set id(username){
+        this.state._id = username;
+    }
+    set lat(latitude){
+        this.state._lat = latitude;
+    }
+    set lon(longitude){
+        this.state._lon = longitude;
+    }
+    get id(){
+        return this.state._id;
+    }
+    get lat(){
+        return this.state._lat;
+    }
+    get lon(){
+        return this.state._lon;
+    }
+}
+
+
+function FirstMenu() {
+    return (
+        <div>
+            <form>
+                <label for="username">Input Username: </label>
+                <input type="text" id="username" name="username"></input> <br />
+                <label for="address">Input Address: </label>
+                <input type="text" id="address" name="address"></input>
+            </form>
+        </div>
+    )
+}
+
+export { User, FirstMenu }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { User, FirstMenu } from './MainPage.js'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirstMenu />
   </React.StrictMode>,
   document.getElementById('root')
 );
